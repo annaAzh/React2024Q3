ffa26d2873a2eb7b1b614fe9c4ce378dd1243e79
annaAzh <forarre@yandex.ru>
2024-07-02 15:21:59 +0300

    refactor: delete template code

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import { SearchPage } from '../pages/SearchPage';
+import './styles/variables/global.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <SearchPage />;
+  }
+}
+
+export default App;

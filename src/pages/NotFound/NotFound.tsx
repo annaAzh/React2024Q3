5919a6a5b4baafae5f0c89ec44fe96db72b39284
annaAzh <forarre@yandex.ru>
2024-07-14 11:44:12 +0300

    feat: add styles for not found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,10 @@
+import style from './NotFound.module.scss';
+
 const NotFound = () => {
   return (
     <div>
-      <h2>Not Found Page</h2>
+      <h1 className={style.title_404}>404</h1>
+      <h2 className={style.title}>Not Found Page</h2>
     </div>
   );
 };

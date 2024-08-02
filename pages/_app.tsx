a5b4fad93b0e5594bfe2c0b16b776ee3df07a6cf
annaAzh <forarre@yandex.ru>
2024-08-02 15:38:41 +0300

    feat: add new app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import React, { FC } from 'react';
-import '../styles/global.css';
 import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { wrapper } from 'app/providers/storeProvider/config/wrapper';
+import { ThemeProvider } from 'app/providers/themeProvider/Themecontext';
+import '../src/app/styles/global.scss';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const { store } = wrapper.useWrappedStore(pageProps);
+
+  return (
+    <Provider store={store}>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
+  );
 };
 
 export default MyApp;

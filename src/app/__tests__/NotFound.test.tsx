dbc5816abb4a7fdcdfb91d19b0bba708131f763e
annaAzh <forarre@yandex.ru>
2024-08-08 16:29:25 +0300

    feat: update notFound and layout pages

--- src/app/__tests__/NotFound.test.tsx
+++ src/app/__tests__/NotFound.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
-import { NotFound } from './NotFound';
-import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from 'shared/lib/__mock__';
+import { ThemeProvider } from 'app/providers/themeProvider/Themecontext';
+import Custom404 from '../../../pages/404';
 
 describe('NotFound Component', () => {
   it('render not found page', async () => {
     render(
-      <MemoryRouter>
-        <NotFound />
-      </MemoryRouter>,
+      <Provider store={store}>
+        <ThemeProvider>
+          <Custom404 />
+        </ThemeProvider>
+      </Provider>,
     );
 
     const noResultsMessage = await screen.findByText(/Not found page/i);
--- src/widget/Layout/Layout.test.tsx
+++ src/widget/Layout/Layout.test.tsx
@@ -1,15 +1,15 @@
 import { render, screen } from '@testing-library/react';
-import { ThemeContext } from 'app/store/Themecontext';
 import { describe, expect, it, vi } from 'vitest';
 import { Layout } from './Layout';
 import style from './Layout.module.scss';
+import { ThemeContext, ThemeContextType } from 'app/providers/themeProvider/Themecontext';
 
 describe('Layout component', () => {
   it('renders layout with appropriate class based on isDarkMode', () => {
-    const mockContextValue = { isDarkMode: true, setIsDarkMode: vi.fn() };
+    const mockContextValue: ThemeContextType = { isDarkMode: true, toggleTheme: vi.fn() };
     render(
       <ThemeContext.Provider value={mockContextValue}>
-        <Layout />
+        <Layout>hello</Layout>
       </ThemeContext.Provider>,
     );
 
@@ -20,10 +20,10 @@ describe('Layout component', () => {
   });
 
   it('renders layout with appropriate class based on not isDarkMode', () => {
-    const mockContextValue = { isDarkMode: false, setIsDarkMode: vi.fn() };
+    const mockContextValue: ThemeContextType = { isDarkMode: false, toggleTheme: vi.fn() };
     render(
       <ThemeContext.Provider value={mockContextValue}>
-        <Layout />
+        <Layout>hello</Layout>
       </ThemeContext.Provider>,
     );
 

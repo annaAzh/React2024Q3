7be63caba717139441f3e940b44155a258b5e041
annaAzh <forarre@yandex.ru>
2024-08-08 16:27:21 +0300

    chore: set up dependencies for testing

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@ import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   distDir: './dist',
   sassOptions: {
     includePaths: [path.join(process.cwd(), 'styles')],
+  },
+  images: {
+    unoptimized: true,
   },
 };
 

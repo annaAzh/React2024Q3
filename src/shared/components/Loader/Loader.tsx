dbdbe7944a246b346340bdb1ba02e0474a48a9b0
annaAzh <forarre@yandex.ru>
2024-08-08 16:41:48 +0300

    fix: add priority attribute at loader image

--- src/shared/components/Loader/Loader.tsx
+++ src/shared/components/Loader/Loader.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 const Loader: FC = () => {
   return (
     <div className={style.loader}>
-      <Image className={style.loader_img} width={100} height={100} src={loader.src} alt="loader" />
+      <Image className={style.loader_img} width={100} height={100} src={loader} alt="loader" priority />
     </div>
   );
 };

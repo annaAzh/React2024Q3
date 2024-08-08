df447559ede265fc72c2a853f66db3117843bd31
annaAzh <forarre@yandex.ru>
2024-08-08 16:45:52 +0300

    fix: update tests for loader and toggle button

--- src/shared/components/Loader/Loader.test.tsx
+++ src/shared/components/Loader/Loader.test.tsx
@@ -1,12 +1,10 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { Loader } from './Loader';
 import createFetchMock from 'vitest-fetch-mock';
-import { SearchPage } from 'pages';
 import { Provider } from 'react-redux';
-import { SearchResponse } from 'shared/types';
 import { store } from 'shared/lib/__mock__';
+import { ThemeProvider } from 'app/providers/themeProvider/Themecontext';
 
 const fetchMock = createFetchMock(vi);
 fetchMock.enableMocks();
@@ -18,53 +16,14 @@ describe('Component Loader', () => {
 
   it('renders loader', () => {
     render(
-      <MemoryRouter>
-        <Loader />
-      </MemoryRouter>,
+      <Provider store={store}>
+        <ThemeProvider>
+          <Loader />
+        </ThemeProvider>
+      </Provider>,
     );
 
     const loader = screen.getByAltText('loader');
     expect(loader).toBeInTheDocument();
-  });
-
-  it('loading indicator is displayed while fetching data', async () => {
-    const mockResponse: SearchResponse = {
-      results: [
-        {
-          id: 1,
-          name: 'Rick',
-          status: 'Alive',
-          species: 'Human',
-          type: '',
-          gender: 'Male',
-          origin: { name: 'Earth' },
-          location: { name: 'Mars' },
-          image: 'string',
-          episode: ['Episode 1', 'Episode 2'],
-          url: 'string',
-          created: '2021-01-01T00:00:00.000Z',
-        },
-      ],
-      info: { count: 1, next: null, prev: null, pages: 1 },
-    };
-
-    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
-
-    render(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={['/']}>
-          <Routes>
-            <Route path="/" element={<SearchPage />} />
-          </Routes>
-        </MemoryRouter>
-      </Provider>,
-    );
-
-    expect(screen.getByAltText(/loader/i)).toBeInTheDocument();
-
-    await waitFor(() => {
-      expect(screen.queryByText(/loader/i)).not.toBeInTheDocument();
-      expect(screen.getByText(/rick/i)).toBeInTheDocument();
-    });
   });
 });
--- src/shared/components/ToggleButton/ToggleButton.test.tsx
+++ src/shared/components/ToggleButton/ToggleButton.test.tsx
@@ -1,13 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { describe, it, expect, vi } from 'vitest';
-import { ThemeContext } from 'app/store/Themecontext';
 import { ToggleButton } from './ToggleButton';
 import style from './ToggleButton.module.scss';
 import userEvent from '@testing-library/user-event';
+import { ThemeContext, ThemeContextType, ThemeProvider } from 'app/providers/themeProvider/Themecontext';
 
 describe('ToggleButton component', () => {
   it('renders button with appropriate class based on isDarkMode', () => {
-    const mockContextValue = { isDarkMode: true, setIsDarkMode: vi.fn() };
+    const mockContextValue: ThemeContextType = { isDarkMode: true, toggleTheme: vi.fn() };
     render(
       <ThemeContext.Provider value={mockContextValue}>
         <ToggleButton />
@@ -20,21 +20,32 @@ describe('ToggleButton component', () => {
   });
 
   it('toggles dark mode on button click', async () => {
-    const setIsDarkModeMock = vi.fn();
-    const mockContextValue = { isDarkMode: true, setIsDarkMode: setIsDarkModeMock };
-    render(
-      <ThemeContext.Provider value={mockContextValue}>
-        <ToggleButton />
-      </ThemeContext.Provider>,
-    );
+    const TestComponent = () => {
+      return (
+        <ThemeProvider>
+          <ToggleButton />
+        </ThemeProvider>
+      );
+    };
+
+    render(<TestComponent />);
 
     const button = screen.getByRole('button');
+    expect(button).not.toHaveClass('on');
+
     await userEvent.click(button);
-    expect(setIsDarkModeMock).toHaveBeenCalledWith(false);
+    waitFor(() => {
+      expect(button).toHaveClass('on');
+    });
+
+    await userEvent.click(button);
+    waitFor(() => {
+      expect(button).not.toHaveClass('on');
+    });
   });
 
   it('renders button with appropriate class when isDarkMode is false', () => {
-    const mockContextValue = { isDarkMode: false, setIsDarkMode: vi.fn() };
+    const mockContextValue: ThemeContextType = { isDarkMode: false, toggleTheme: vi.fn() };
     render(
       <ThemeContext.Provider value={mockContextValue}>
         <ToggleButton />
@@ -44,19 +55,5 @@ describe('ToggleButton component', () => {
     const button = screen.getByRole('button');
     expect(button).toBeInTheDocument();
     expect(button).toHaveClass(style.button);
-  });
-
-  it('toggles light mode on button click', async () => {
-    const setIsDarkModeMock = vi.fn();
-    const mockContextValue = { isDarkMode: false, setIsDarkMode: setIsDarkModeMock };
-    render(
-      <ThemeContext.Provider value={mockContextValue}>
-        <ToggleButton />
-      </ThemeContext.Provider>,
-    );
-
-    const button = screen.getByRole('button');
-    await userEvent.click(button);
-    expect(setIsDarkModeMock).toHaveBeenCalledWith(true);
   });
 });

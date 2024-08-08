a1859a209af99e40185e9897d568022112928105
annaAzh <forarre@yandex.ru>
2024-08-08 16:42:28 +0300

    feat: update pagination component and tests

--- src/widget/Pagination/ui/Pagination.test.tsx
+++ src/widget/Pagination/ui/Pagination.test.tsx
@@ -1,17 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter, Route, Routes, useSearchParams } from 'react-router-dom';
+import { render, screen, waitFor } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import { Pagination } from './Pagination';
 import userEvent from '@testing-library/user-event';
+import { Provider } from 'react-redux';
+import { store } from 'shared/lib/__mock__';
+import { ThemeProvider } from 'app/providers/themeProvider/Themecontext';
+import { useState } from 'react';
+
+vi.mock('next/router', () => ({
+  useRouter: () => ({
+    push: vi.fn(),
+    replace: vi.fn(),
+    query: {},
+  }),
+}));
 
 const onChangePage = vi.fn();
 
 describe('Pagination Component', () => {
   it('render pagination component', () => {
     render(
-      <MemoryRouter>
-        <Pagination totalPage={10} currentPage={1} siblings={1} onChangePage={onChangePage} />
-      </MemoryRouter>,
+      <Provider store={store}>
+        <ThemeProvider>
+          <Pagination totalPage={10} currentPage={1} siblings={1} onChangePage={onChangePage} />
+        </ThemeProvider>
+      </Provider>,
     );
 
     const paginationItems = screen.getAllByRole('button');
@@ -20,8 +33,7 @@ describe('Pagination Component', () => {
 
   it('updates URL query parameter when page changes', async () => {
     const TestComponent = () => {
-      const [searchParams, setSearchParams] = useSearchParams();
-      const currentPage = Number(searchParams.get('page') ?? 1);
+      const [currentPage, setCurrentPage] = useState(1);
 
       return (
         <>
@@ -30,8 +42,8 @@ describe('Pagination Component', () => {
             currentPage={currentPage}
             siblings={1}
             onChangePage={(page) => {
-              setSearchParams({ page: page.toString() });
               onChangePage(page);
+              setCurrentPage(page);
             }}
           />
           <div data-testid="current-page">{currentPage}</div>
@@ -40,11 +52,11 @@ describe('Pagination Component', () => {
     };
 
     render(
-      <MemoryRouter initialEntries={['/?page=1']}>
-        <Routes>
-          <Route path="/" element={<TestComponent />} />
-        </Routes>
-      </MemoryRouter>,
+      <Provider store={store}>
+        <ThemeProvider>
+          <TestComponent />
+        </ThemeProvider>
+      </Provider>,
     );
 
     expect(screen.getByTestId('current-page').textContent).toBe('1');
@@ -63,5 +75,45 @@ describe('Pagination Component', () => {
     await userEvent.click(previousPageButtons[0]);
     expect(screen.getByTestId('current-page').textContent).toBe('1');
     expect(onChangePage).toHaveBeenCalledWith(1);
+  });
+
+  it('handle prev click button', () => {
+    const TestComponent = () => {
+      const [currentPage, setCurrentPage] = useState(5);
+
+      return (
+        <>
+          <Pagination
+            totalPage={10}
+            currentPage={currentPage}
+            siblings={1}
+            onChangePage={(page) => {
+              onChangePage(page);
+              setCurrentPage(page);
+            }}
+          />
+          <div data-testid="current-page">{currentPage}</div>
+        </>
+      );
+    };
+
+    render(
+      <Provider store={store}>
+        <ThemeProvider>
+          <TestComponent />
+        </ThemeProvider>
+      </Provider>,
+    );
+
+    const prevArrow = screen.getByTestId(/prevArrow/i);
+    expect(prevArrow).toBeInTheDocument();
+    userEvent.click(prevArrow);
+    waitFor(() => {
+      expect(onChangePage).toHaveBeenCalledWith(4);
+      expect(screen.getByTestId('current-page').textContent).toBe('4');
+
+      const activeBtn = screen.getByRole('button', { name: /4/i });
+      expect(activeBtn).toHaveClass(/active/i);
+    });
   });
 });
--- src/widget/Pagination/ui/Pagination.tsx
+++ src/widget/Pagination/ui/Pagination.tsx
@@ -1,8 +1,9 @@
-import { FC, useContext } from 'react';
+import { FC } from 'react';
 import style from './Pagination.module.scss';
 import arrow from 'assets/icons/arrow-left.svg';
 import { getPaginationArray } from 'shared/utils/helpers';
-import { ThemeContext } from 'app/store/Themecontext';
+import Image from 'next/image';
+import { useTheme } from 'app/providers/themeProvider/hook';
 
 type PaginationProps = {
   totalPage: number;
@@ -13,18 +14,19 @@ type PaginationProps = {
 
 const Pagination: FC<PaginationProps> = (props) => {
   const { currentPage, totalPage, siblings, onChangePage } = props;
-  const { isDarkMode } = useContext(ThemeContext);
+  const { isDarkMode } = useTheme();
 
   const arr = getPaginationArray(totalPage, currentPage, siblings);
 
   return (
     <div className={style.block}>
       <button
+        data-testid="prevArrow"
         className={isDarkMode ? `${style.arrow_left} ${style.arrow_left_dark}` : style.arrow_left}
         disabled={currentPage === 1}
         onClick={() => onChangePage(currentPage > 1 ? currentPage - 1 : currentPage)}
       >
-        <img className={style.arrow_img} src={arrow} />
+        <Image className={style.arrow_img} src={arrow} width="40" height="40" alt="arrow" />
       </button>
 
       {arr.map((el, i) => (
@@ -54,11 +56,12 @@ const Pagination: FC<PaginationProps> = (props) => {
       ))}
 
       <button
+        data-testid="next-arrow"
         className={isDarkMode ? `${style.arrow_right} ${style.arrow_right_dark}` : style.arrow_right}
         disabled={currentPage === totalPage}
         onClick={() => onChangePage(currentPage < totalPage ? currentPage + 1 : currentPage)}
       >
-        <img className={style.arrow_next_img} src={arrow} />
+        <Image className={style.arrow_next_img} src={arrow} width="40" height="40" alt="arrow" />
       </button>
     </div>
   );

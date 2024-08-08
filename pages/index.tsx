0bda245a21f17074899fba0f0cf55749564fefd3
annaAzh <forarre@yandex.ru>
2024-08-08 16:33:07 +0300

    feat: create home page and default page with loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,24 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import React from 'react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { getLocaleStorage } from 'shared/utils/localeStorage/LocaleStorage';
+import { Loader } from 'shared/components/Loader/Loader';
+import { Layout } from 'widget/Layout/Layout';
+import { Paths } from 'shared/types';
 
-const Home = () => {
+const DefaultPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const storedValue = getLocaleStorage() || '';
+
+    router.push(`/${Paths.hero}?search=${encodeURIComponent(storedValue)}`);
+  }, []);
+
   return (
-    <div>
-      <Head>
-        <title>React2024Q3</title>
-        <meta name="react homework" content="rs school" />
-      </Head>
-    </div>
+    <Layout>
+      <Loader />
+    </Layout>
   );
 };
 
-export default Home;
+export default DefaultPage;

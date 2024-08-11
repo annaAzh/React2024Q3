acb5dd6adc56395bf54c46b6bd807d3c7737dc08
annaAzh <forarre@yandex.ru>
2024-08-11 17:05:56 +0300

    feat: update test for hero page

--- src/pages/Hero/Hero.test.tsx
+++ src/pages/Hero/Hero.test.tsx
@@ -1,86 +1,31 @@
+import { expect, it } from 'vitest';
+import { createRemixStub } from '@remix-run/testing';
 import { render } from '@testing-library/react';
-import { afterAll, beforeAll, describe, expect, it } from 'vitest';
-import { MemoryRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
-import { Hero } from './Hero';
-import { http, HttpResponse } from 'msw';
-import { setupServer } from 'msw/node';
-import { Provider } from 'react-redux';
-import { store } from 'shared/lib/__mock__';
+import { ThemeProvider } from 'app/providers/themeProvider/Themecontext';
+import HeroPage, { loader } from 'app/routes/heroes.$id';
+import { json } from '@remix-run/node';
+import { heroes } from 'shared/lib/__mock__';
 
-const heroes = [
-  {
-    id: 1,
-    name: 'Rick',
-    status: 'Alive',
-    species: 'Human',
-    type: '',
-    gender: 'Male',
-    origin: { name: 'Earth' },
-    location: { name: 'Mars' },
-    image: 'string',
-    episode: ['Episode 1', 'Episode 2'],
-    url: 'string',
-    created: '2021-01-01T00:00:00.000Z',
-  },
-  {
-    id: 2,
-    name: 'Morty',
-    status: 'Alive',
-    species: 'Human',
-    type: '',
-    gender: 'Male',
-    origin: { name: 'Earth' },
-    location: { name: 'Mars' },
-    image: 'string',
-    episode: ['Episode 1', 'Episode 2'],
-    url: 'string',
-    created: '2021-01-01T00:00:00.000Z',
-  },
-];
+it('testing Details server component', async () => {
+  const MockDetailsPage = createRemixStub([
+    {
+      path: `/`,
+      Component: HeroPage,
+      loader(): Awaited<ReturnType<typeof loader>> {
+        return json({ data: heroes[0] });
+      },
+    },
+  ]);
 
-export const BASE_URL: string = 'https://rickandmortyapi.com/api/character';
-export const BASE_URL_HERO: string = 'https://rickandmortyapi.com/api/character/1';
+  const { findByTestId, findByText } = render(
+    <ThemeProvider>
+      <MockDetailsPage />
+    </ThemeProvider>,
+  );
 
-const handlers = [
-  http.get(`${BASE_URL}`, async () => {
-    return HttpResponse.json(heroes[0]);
-  }),
-  http.get(`${BASE_URL_HERO}`, async () => {
-    return HttpResponse.json(heroes[0]);
-  }),
-];
+  const close = await findByTestId(/close/i);
+  expect(close).toBeInTheDocument();
 
-const server = setupServer(...handlers);
-
-describe('Component Hero', () => {
-  beforeAll(() => {
-    server.listen();
-  });
-
-  afterAll(() => {
-    server.close();
-  });
-
-  it('testing test', async () => {
-    const { getByText, findByText, getByTestId } = render(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={['/heroes/1']}>
-          <Hero />
-        </MemoryRouter>
-        ,
-      </Provider>,
-    );
-
-    const loading = getByText(/loading.../i);
-    expect(loading).toBeInTheDocument();
-    const name = await findByText(heroes[0].name);
-    expect(name).toBeInTheDocument();
-    expect(loading).not.toBeInTheDocument();
-
-    const closeBtn = getByTestId(/close/i);
-    expect(closeBtn).toBeInTheDocument();
-
-    await userEvent.click(closeBtn);
-  });
+  const details = await findByText(/Rick/i);
+  expect(details).toBeInTheDocument();
 });

ce6dd2eb2651b597d1c327a145b84300b0aa8cd2
annaAzh <forarre@yandex.ru>
2024-08-12 19:38:24 +0300

    chore: set up ESLint to throw errors for any type

--- eslint.config.js
+++ eslint.config.js
@@ -22,6 +22,7 @@ export default tseslint.config({
   rules: {
     ...reactHooks.configs.recommended.rules,
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-explicit-any': 'error',
     ...prettierConfig.rules,
   },
 });

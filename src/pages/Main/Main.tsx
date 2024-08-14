63cf57c110c8c997fb3444728150f280914dfdfa
annaAzh <forarre@yandex.ru>
2024-08-14 15:25:17 +0300

    fix: fix styles for links

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,9 +4,9 @@ import { Path } from 'shared/types/routePaths';
 export const Main = () => {
   return (
     <>
-      <h1>Main page</h1>
       <Link to={Path.controlFrom}>Controll form</Link>
       <Link to={Path.unControlFrom}>Un Controll form</Link>
+      <h1>Main page</h1>
     </>
   );
 };
